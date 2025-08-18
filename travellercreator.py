01f0571da1d4cb8a465ece3506fe8473bc1b06ef
RealJadefalke <dennismross@live.de>
2025-08-18 21:33:08 +0200

    August1825

--- travellercreator.py
+++ travellercreator.py
@@ -1,6 +1,6 @@
 import random
 
-
+#Dictionary um die sechs Grundcharakteristika für den Charakter zu speichern
 attribute = {
   "Stärke" : None,
   "Geschicklichkeit" : None,
@@ -11,7 +11,12 @@ attribute = {
   }
 
 
+laufbahn = None #Laufbahn des Charakters
+eingezogen = False #Variable um festzustellen ob der Charakter eingezogen wurde
+dienstrang = None #Dienstrang des Charakters
+offizierspatent = False #Hat der Charakter ein Offizierspatent oder nicht
 
+#Grundcharakteristika auswürfeln
 def grundwerte():
   attribute["Stärke"] = random.randrange(2, 12)
   attribute["Geschicklichkeit"] = random.randrange(2, 12)
@@ -20,73 +25,211 @@ def grundwerte():
   attribute["Bildung"] = random.randrange(2, 12)
   attribute["Sozialstatus"] = random.randrange(2, 12)
 
+#Charakterlaufbahn festlegen
 def einstellung(x):
-  if x == 1 :
-    wurf = random.randrange(2,12)
-    print(wurf)
-    if attribute["Intelligenz"] >= 8 :
-      wurf += 1
-    if attribute["Bildung"] >= 9 :
-      wurf += 2
-    if wurf >= 8 :
-      print("Erfolg")
-    else :
-     print("Leider nicht!")
-  elif x == 2:
-    wurf = random.randrange(2,12)
-    print(wurf)
-    if attribute["Intelligenz"] >= 8 :
-      wurf += 1
-    if attribute["Stärke"] >= 8 :
-      wurf += 2
-    if wurf >= 9 :
-      print("Erfolg")
-    else :
-     print("Leider nicht!")
-  elif x == 3:
-    wurf = random.randrange(2,12)
-    print(wurf)
-    if attribute["Geschicklichkeit"] >= 6 :
-      wurf += 1
-    if attribute["Ausdauer"] >= 5 :
-      wurf += 2
-    if wurf >= 5 :
-      print("Erfolg")
-    else :
-     print("Leider nicht!")
-  elif x == 4:
-    wurf = random.randrange(2,12)
-    print(wurf)
-    if attribute["Intelligenz"] >= 6 :
-      wurf += 1
-    if attribute["Stärke"] >= 8 :
-      wurf += 2
-    if wurf >= 7 :
-      print("Erfolg")
-    else :
-     print("Leider nicht!")
-  elif x == 5:
-    wurf = random.randrange(2,12)
-    print(wurf)
-    if attribute["Stärke"] >= 7 :
-      wurf += 1
-    if attribute["Intelligenz"] >= 6 :
-      wurf += 2
-    if wurf >= 7 :
-      print("Erfolg")
-    else :
-     print("Leider nicht!")
+  wurf = random.randrange(2,12)
+  print(f"Dein Wurf:{wurf}")
+  match x :
+    case 1 : #Raumflotte
+      if attribute["Intelligenz"] >= 8 :
+        wurf += 1
+      if attribute["Bildung"] >= 9 :
+        wurf += 2
+      if wurf >= 8 :
+        print("Erfolg")
+        return "Raumflotte"
+      else :
+        print("Leider nicht!")
+        return None
+    case 2: #Raumgarde
+      if attribute["Intelligenz"] >= 8 :
+        wurf += 1
+      if attribute["Stärke"] >= 8 :
+        wurf += 2
+      if wurf >= 9 :
+        print("Erfolg")
+        return "Raumgarde"
+      else :
+        print("Leider nicht!")
+        return None
+    case 3: #Heer
+      if attribute["Geschicklichkeit"] >= 6 :
+        wurf += 1
+      if attribute["Ausdauer"] >= 5 :
+        wurf += 2
+      if wurf >= 5 :
+        print("Erfolg")
+        return "Heer"
+      else :
+        print("Leider nicht!")
+        return None
+    case 4: #Scoutdienst
+      if attribute["Intelligenz"] >= 6 :
+        wurf += 1
+      if attribute["Stärke"] >= 8 :
+        wurf += 2
+      if wurf >= 7 :
+        print("Erfolg")
+        return "Scoutdienst"
+      else :
+        print("Leider nicht!")
+        return None
+    case 5: #Handelsflotte
+      if attribute["Stärke"] >= 7 :
+        wurf += 1
+      if attribute["Intelligenz"] >= 6 :
+        wurf += 2
+      if wurf >= 7 :
+        print("Erfolg")
+        return "Handelsflotte"
+      else :
+        print("Leider nicht!")
+        return None
+    case 6: #Andere
+      if wurf >= 3:
+        print("Erfolg")
+        return "Andere"
+      else:
+        print("Leider nicht!") 
+        return None
+
+#Bei abgelehntem Karrierewurf Einziehungswurf
+def einziehung(eingezogen) :
+  wurf = random.randrange(1,6)
+  match wurf:
+    case 1:
+      eingezogen = True
+      return "Raumflotte"
+    case 2:
+      eingezogen = True
+      return "Raumgarde"
+    case 3:
+      eingezogen = True
+      return "Heer"
+    case 4:
+      eingezogen = True
+      return "Scoutdienst"
+    case 5:
+      eingezogen = True
+      return "Handelsflotte"
+    case 6:
+      eingezogen = True
+      return "Andere"
+
+
+#Überlebenswurf um festzustellen ob der Charakter überlebt
+def ueberlebt(x) :
+  wurf = random.randrange(2,12)
+  match x:
+    case "Raumflotte":
+      if attribute["Intelligenz"] >= 7 :
+        wurf += 2
+      if wurf >= 5 :
+        print("Überlebt!")
+      else:
+        print("Leider gestorben! Versuch es nochmal!")
+        quit()
+    case "Raumgarde":
+      if attribute["Ausdauer"] >= 8 :
+        wurf += 2
+      if wurf >= 6 :
+        print("Überlebt!")
+      else:
+        print("Leider gestorben! Versuch es nochmal!")
+        quit()
+    case "Heer":
+      if attribute["Bildung"] >= 6 :
+        wurf += 2
+      if wurf >= 5 :
+        print("Überlebt!")
+      else:
+        print("Leider gestorben! Versuch es nochmal!")
+        quit()
+    case "Scoutdienst":
+      if attribute["Ausdauer"] >= 9 :
+        wurf += 2
+      if wurf >= 7 :
+        print("Überlebt!")
+      else:
+        print("Leider gestorben! Versuch es nochmal!")
+        quit()
+    case "Handelsflotte":
+      if attribute["Intelligenz"] >= 7 :
+        wurf += 2
+      if wurf >= 5 :
+        print("Überlebt!")
+      else:
+        print("Leider gestorben! Versuch es nochmal!")
+        quit()
+    case "Andere":
+      if attribute["Intelligenz"] >= 9 :
+        wurf += 2
+      if wurf >= 5 :
+        print("Überlebt!")
+      else:
+        print("Leider gestorben! Versuch es nochmal!")
+        quit()
+
+
+#Beförderungswurf in der ersten Periode nur möglich wenn nicht eingezogen
+def befoerderung() :
+  if eingezogen == True :
+    eingezogen = False
   else:
-    wurf = random.randrange(2,12)
-    if wurf >= 3:
-      print("Erfolg")
-    else:
-      print("Leider nicht!")
-    
- 
+    if offizierspatent == False:
+      wurf = random.randrange(2,12)
+      match laufbahn:
+        case "Raumflotte":
+          if attribute["Sozialstatus"] >=9 :
+            wurf +=1
+          if wurf >= 10:
+            offizierspatent == True
+            print("Glückwunsch zum Offizierspatent!")
+          else:
+            print("Leider kein Patent, versuch es nächstes Jahr nochmal!")
+        case "Raumgarde":
+          if attribute["Bildung"] >=7 :
+            wurf +=1
+          if wurf >= 9:
+            offizierspatent == True
+            print("Glückwunsch zum Offizierspatent!")
+          else:
+            print("Leider kein Patent, versuch es nächstes Jahr nochmal!")
+        case "Heer":
+          if attribute["Ausdauer"] >=9 :
+            wurf +=1
+          if wurf >= 5:
+            offizierspatent == True
+            print("Glückwunsch zum Offizierspatent!")
+          else:
+            print("Leider kein Patent, versuch es nächstes Jahr nochmal!")
+        case "Scoutdienst":
+          pass
+        case "Handelsflotte":
+          if attribute["Intelligenz"] >=6 :
+            wurf +=1
+          if wurf >= 4:
+            offizierspatent == True
+            print("Glückwunsch zum Offizierspatent!")
+          else:
+            print("Leider kein Patent, versuch es nächstes Jahr nochmal!")
+        case "Andere":
+          pass
+
+
+
+
+
+
+
+
+
 
 print("Willkommen beim TRAVELLER Generator")
 grundwerte()
+print("Dein UPP:")
+for key, value in attribute.items() :
+  print (f"{key:<17}: {value:>2}")
 print("Wähle deine Dienstgattung:")
 print("[1] Raumflotte")
 print("[2] Raumgarde")
@@ -94,6 +237,9 @@ print("[3] Heer")
 print("[4] Scoutdienst")
 print("[5] Handelsflotte")
 print("[6] Andere")
-einstellung(int(input()))
-print(attribute)
-#print(dienstgattung)
+laufbahn = einstellung(int(input()))
+if laufbahn == None :
+  laufbahn = einziehung()
+print(laufbahn)
+ueberlebt(laufbahn)
+befoerderung()
